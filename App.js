c373d076e1f7cf277fc288c5e22152e4b3573498
apple <ratnadeep.konar@gmail.com>
2019-07-13 19:52:22 +0530

    Add basic app

--- App.js
+++ App.js
@@ -1,40 +1,23 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React from 'react'
+import { View, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import { white, black } from './utils/colors'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+
+const store = createStore(reducer)
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Change code in the editor and watch it change on your phone! Save to get a shareable url.
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
-      </View>
-    );
-  }
+    
+    render() {
+        return (
+            <Provider store={createStore(reducer)}>
+                <View style={{ flex: 1 }}>
+                    Hi! welcome.
+                </View>
+            </Provider>
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
